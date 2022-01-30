de0b765695f524137d11e51a0c1ffb599ee23147
prudhvirdy25 <prudhvi.rdy25@gmail.com>
2022-01-30 18:26:26 -0500

    Modified

--- CodeSnippets/27_Lists.py
+++ CodeSnippets/27_Lists.py
@@ -36,3 +36,36 @@ listnested=[["Prudhvi","Reddy"],"Nallavadla"]
 print(listnested)
 print(listnested[0])
 print(listnested[1])
+
+#changes to use insert() method
+
+listinsert=[1,3,7,9]
+# Addition of Element at
+# specific Position
+# (using Insert Method)
+listinsert.insert(3,"Prudhvi")
+print("\n  list after insert operation")
+print(listinsert)
+
+#changes to use extend() method
+listextend=[1,3,4,5,6]
+
+listextend.extend(["Prudhvi","Reddy","N",1,2,3,4])
+print("\n  list after extend operation")
+print(listextend)
+
+# negative Indexing to get the elements from the end
+print(listextend[-2])
+
+# remove the elements 
+List = [1, 2, 3, 4, 5, 6,
+        7, 8, 9, 10, 11, 12]
+print("Initial List: ")
+print(List)
+ 
+# Removing elements from List
+# using Remove() method
+List.remove(5)
+List.remove(6)
+print("\nList after Removal of two elements: ")
+print(List)
