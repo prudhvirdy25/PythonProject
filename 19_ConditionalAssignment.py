c385dd3f21798d87e2d31063a9e6a0bb7c9e0f7f
prudhvirdy25 <prudhvi.rdy25@gmail.com>
2022-01-30 00:48:58 -0500

    commit ondelete

--- 19_ConditionalAssignment.py
+++
@@ -1,8 +0,0 @@
-# Python3 code to demonstrate variable assignment
-# upon condition using Conditional Operator
-
-# Initialising variables using Conditional Operator
-a = 1 if 20 > 10 else 0
-
-# Printing value of a
-print("The value of a is: " + str(a))
